53bf7718dc113b42fcca49154e3f2d576f39db6f
Mr-Muneeb101 <160252527+Mr-Muneeb101@users.noreply.github.com>
2024-07-20 12:00:30 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function atm() {
+    const pin = 123;
+    let currentAmount = 5000;
+    let user_inputs = await inquirer.prompt([
+        {
+            name: "pinNumber",
+            type: "input",
+            message: "enter the pin",
+            validate: (number) => {
+                if (number !== pin.toString()) {
+                    return "Wrong Pin";
+                }
+                else {
+                    return true;
+                }
+            }
+        }, {
+            name: "operation",
+            type: "list",
+            choices: ["Withdraw", "Check balance"],
+        },
+    ]);
+    if (user_inputs.operation === "Withdraw") {
+        let User_input_amount = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "input",
+                message: "Enter the amount for withdraw",
+                validate: (input) => {
+                    if (input) {
+                        if (input < currentAmount) {
+                            return true;
+                        }
+                        else {
+                            console.log();
+                            return `\n your current ammount is ${currentAmount} : \n \t Enter valid ammount please\n`;
+                        }
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+        ]);
+        currentAmount = currentAmount - User_input_amount.amount;
+        console.log(`Your ammount have been withdraw: \n your current ammount is: ${currentAmount}`);
+    }
+    else if (user_inputs.operation === "Check balance") {
+        console.log(`Your current amount is: ` + currentAmount);
+    }
+}
+atm();
